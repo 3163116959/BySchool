46cd200f0372cb1d70533fec73104c7f0e12a806
xf <3163116959@qq.com>
2020-08-20 22:37:32 +0800

    更新了一些常用的sql语句，还有过程说明

---
+++ src/main/java/com/mapper/suSheMapper.java
@@ -0,0 +1,7 @@
+package com.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.entity.suShe;
+
+public interface suSheMapper extends BaseMapper<suShe> {
+}
---
+++ src/main/java/com/mapper/superPaMapper.java
@@ -0,0 +1,7 @@
+package com.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.entity.superPa;
+
+public interface superPaMapper extends BaseMapper<superPa> {
+}
